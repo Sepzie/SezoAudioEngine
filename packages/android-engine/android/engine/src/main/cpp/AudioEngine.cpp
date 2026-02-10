452c7a9b6c171dda389ddfbc0fbbbdd43a0f0ae6
Sepehr Zohoori Rad <zohooris1@gmail.com>
2026-02-09 23:08:06 -0800

    fix error

--- packages/android-engine/android/engine/src/main/cpp/AudioEngine.cpp
+++ packages/android-engine/android/engine/src/main/cpp/AudioEngine.cpp
@@ -7,6 +7,7 @@
 
 #define LOG_TAG "AudioEngine"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 namespace sezo {
